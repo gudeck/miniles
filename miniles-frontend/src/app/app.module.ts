5afd36aa009ae868b379a38ed65752d201761045
Gustavo Zucolotto <guzuc@outlook.com>
2021-08-16 23:02:48 -0300

    iniciando projeto

---
+++ miniles-frontend/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorModule, SecurityModule, VersionTagModule } from '@nuvem/angular-base';
+import { BreadcrumbModule, ErrorStackModule, MenuModule, PageNotificationModule } from '@nuvem/primeng-components';
+import { BlockUIModule } from 'ng-block-ui';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppFooterComponent } from './components/footer/app.footer.component';
+import { AppTopbarComponent } from './components/topbar/app.topbar.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        AppTopbarComponent,
+        AppFooterComponent
+    ],
+    imports: [
+        BlockUIModule.forRoot({
+            message: 'Carregando...'
+        }),
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        SharedModule,
+        HttpClientModule,
+        PageNotificationModule,
+        BreadcrumbModule,
+        ErrorStackModule,
+        ErrorModule,
+        VersionTagModule,
+        SecurityModule.forRoot(environment.auth),
+        MenuModule
+    ],
+    providers: [
+        { provide: LocationStrategy, useClass: HashLocationStrategy }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
