56159f96317c1486689f45e39fcccd249dd1dbe9
OLukasSilva <lukassilvareis1@gmail.com>
2021-08-19 13:48:55 -0300

    Back service

--- miniles-backend/src/main/java/service/DocumentoService.java
+++ miniles-backend/src/main/java/service/DocumentoService.java
@@ -4,38 +4,36 @@ import domain.Documento;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import repository.DocumentoRepository;
+import service.dto.DocumentoDTO;
 import service.erro.DocumentoNaoEncontrado;
 import service.mapper.DocumentoMappper;
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-
 public class DocumentoService {
 
     private final DocumentoRepository documentoRepository;
     private final DocumentoMappper documentoMappper;
 
 
-    public List<DocumentoDTO> obterTodos() {
+    public List<DocumentoDTO>obterTodos(){
         return documentoMappper.toDto(documentoRepository.findAll());
     }
 
-    public DocumentoDTO obterPorId(Long id) {
-        Documento documento = documentoRepository.findById(id).orElseThrow(DocumentoNaoEncontrado::new);
-        return documentoMappper.toDto(documento);
+    public DocumentoDTO obterTodosPorId(Long id){
+     Documento documento = documentoRepository.findById(id).orElseThrow(DocumentoNaoEncontrado::new);
+     return documentoMappper.toDto(documento);
     }
 
-    public DocumentoDTO salvarDocumento(DocumentoDTO documentoDTO) {
+    public DocumentoDTO salvarDocumento(DocumentoDTO documentoDTO){
         Documento documento = documentoMappper.toEntity(documentoDTO);
-        Documento documentoSalvo = documentoRepository.save(documento);
-        return documentoMappper.toDto(documentoSalvo);
+        Documento documentoSalvar = documentoRepository.save(documento);
+        return documentoMappper.toDto(documentoSalvar);
     }
 
-    public void deletar(Long id) {
+    public void deletar(Long id){
         documentoRepository.deleteById(id);
     }
 
-
 }
-
-
--- miniles-backend/src/main/java/service/dto/DocumentoDTO.java
+++ miniles-backend/src/main/java/service/dto/DocumentoDTO.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class DocumentosDTO {
+public class DocumentoDTO {
 
     private long id;
     private String nome;
