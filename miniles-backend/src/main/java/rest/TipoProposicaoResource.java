2bd52406f7e783922d52e3227e78cbbf1ccd1828
Adr <adriel77adr10@gmail.com>
2021-08-20 09:40:57 -0300

    rest criado

---
+++ miniles-backend/src/main/java/rest/TipoProposicaoResource.java
@@ -0,0 +1,44 @@
+package rest;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.dto.TipoProposicaoDTO;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(value = "/api/tipoproposicao")
+@RestController
+@RequiredArgsConstructor
+public class TipoProposicaoResource {
+
+
+
+    @GetMapping
+    public ResponseEntity<TipoProposicaoDTO> exibirTipos(){
+        return ResponseEntity.ok(tipoProposicaoService.findAll());
+    }
+    @GetMapping
+    public ResponseEntity<TipoProposicaoDTO> buscarTipo( @PathVariable("id") long id){
+        return new ResponseEntity<>(tipoProposicaoService.findById(id),HttpStatus.OK);
+    }
+    @PostMapping
+    public  ResponseEntity<TipoProposicaoDTO> adicionarTipo(@RequestBody TipoProposicaoDTO tipoProposicao){
+
+        return ResponseEntity.ok(tipoProposicaoService.save(tipoProposicao));
+    }
+    @PutMapping
+    public ResponseEntity<TipoProposicaoDTO> atualizarTipo(@RequestBody TipoProposicaoDTO tipoProposicao){
+
+        return ResponseEntity.ok(tipoProposcaoService.save(tipoProposicao));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<void> deletarTipo(@PathVariable("id") long id){
+            tipoProposicaoService.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
