77760a621d11448884c26a8dcafdff3124aafc00
OLukasSilva <lukassilvareis1@gmail.com>
2021-08-20 09:23:56 -0300

    Serivice corrigido

--- miniles-backend/src/main/java/service/ProposicaoSerivce.java
+++ miniles-backend/src/main/java/service/ProposicaoSerivce.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 
-public class PreposicaoSerivce {
+public class ProposicaoSerivce {
 
     private final ProposicaoRepository proposicaoRepository;
     private final ProposicaoMapper proposicaoMapper;
